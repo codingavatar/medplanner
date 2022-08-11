0bfe83fad75d1b979d9a689434cb74206ef89aea
Jessica Chen <jessicachen012@gmail.com>
2022-08-11 10:30:58 -0700

    added delete appt functionality

--- src/components/ApptDetails.jsx
+++ src/components/ApptDetails.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ConditionList from './ConditionList.jsx';
 
-export default function ApptDetails({ appts, selectedAppt, setSelectedAppt }) {
+export default function ApptDetails({ appts, setAppts, selectedAppt, setSelectedAppt }) {
   const [date, setDate] = useState('');
   const [questions, setQuestions] = useState('');
 
@@ -19,23 +19,47 @@ export default function ApptDetails({ appts, selectedAppt, setSelectedAppt }) {
     if (selectedAppt.date !== undefined) {
       setDate(new Date(selectedAppt.date).toLocaleString('en-US', options));
     }
+    // updates question field with selected appt's questions
     setQuestions(selectedAppt.questions || '');
   }, [selectedAppt]);
 
+  // saves questions
   const handleSave = function(e) {
+    e.preventDefault();
     let data = {
       'questions': questions
     };
     axios.put(`/appts/${selectedAppt._id}`, data)
       .then((results) => {
+        // update questions locally
         let apptCopy = selectedAppt;
         apptCopy.questions = questions;
         setSelectedAppt(apptCopy);
-        console.log('saved questions');
       })
       .catch((err) => {
         console.log('Error updating questions: ', err);
       })
+  };
+
+  // deletes currently selected appt
+  const handleDelete = function(e) {
+    e.preventDefault();
+    let confirmed = window.confirm('Do you really want to delete this appointment?');
+    if (confirmed) {
+      axios.delete(`/appts/${selectedAppt._id}`)
+        .then(() => {
+          // remove current appt locally
+          let apptsCopy = appts.filter((appt) => appt._id !== selectedAppt._id);
+          console.log('removed appt: ', apptsCopy);
+          setAppts(apptsCopy);
+          setSelectedAppt({});
+        })
+        .catch((err) => {
+          console.log('Error deleting appointment: ', err);
+        });
+    } else {
+      console.log('Appt not deleted');
+    }
   };
 
   return (
@@ -59,10 +83,12 @@ export default function ApptDetails({ appts, selectedAppt, setSelectedAppt }) {
           />
           <br/>
           <button onClick={(e) => handleSave(e)}>Save Questions</button>
+          &nbsp;&nbsp;&nbsp;
+          <button className="delete-appt-button" onClick={(e) => handleDelete(e)}>Delete appointment</button>
           <ConditionList appts={appts} selectedAppt={selectedAppt}/>
         </div>
         :
-        <div>Select an appointment</div>
+        <div>← Select an appointment</div>
       }
     </div>
   );
